b79b590b8bbddd8150a900263061a2aaffc0d136
Pooya Golchian <pooya.golchian@gmail.com>
2019-09-01 18:14:59 +0430

    add question api

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="container" id="app">
       <Header />
-      <QuestionBox />
+
+      <b-container class="bv-example-row">
+        <b-row>
+         <b-col class="m-auto "> 
+              <QuestionBox
+               :currentQuestion="questions[index]"
+               :next="next"
+              
+              
+               />
+          </b-col>
+        </b-row>
+      </b-container>
+
+
+      
   </div>
 </template>
 
@@ -9,14 +24,40 @@
 import Header from './components/Header'
 import QuestionBox from './components/QuestionBox'
 
+
 export default {
+
   name: 'app',
   components: {
     Header,
     QuestionBox,
-  
+  },
+  methods: {
+    next() {
+      this.index++;
 
-  }
+    }
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+    }
+  },
+  mounted: function() {
+    const apiUrl = 'https://opentdb.com/api.php?amount=10&category=27&type=multiple';
+    fetch(apiUrl, {
+      method: 'get'
+    })
+    .then( (response) => { 
+      return response.json()
+      })
+      .then((jsonData) => {
+        this.questions  = jsonData.results;
+      })
+
+    
+  },
 }
 </script>
 
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -2,21 +2,29 @@
     <div class="pt-5">
         <b-jumbotron class="text-center">            
             <template slot="lead">
-                Some question here?
+                    {{currentQuestion.question}} 
             </template>
+
             <hr class="my-4">
             <p>
                 List of answer!
             </p>
 
             <b-button class="mr-2" variant="primary" href="#">Submit</b-button>
-            <b-button variant="success" href="#">Next</b-button>
+            <b-button @click="next" variant="success" href="#">Next</b-button>
         </b-jumbotron>
     </div>
 </template>
-
 <script>
     export default {
 
+        props: {
+            currentQuestion: Object,
+            next: Function,
+        }
     }
 </script>
+
+
+
+
