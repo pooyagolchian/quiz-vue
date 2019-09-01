d1db3971a728f278e2498190e544c3caecc519e3
Pooya Golchian <pooya.golchian@gmail.com>
2019-09-01 12:34:05 +0430

    Update qb vue component

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="pt-5">
-        <h1>Question Box</h1>    
+        <b-jumbotron class="text-center">            
+            <template slot="lead">
+                Some question here?
+            </template>
+            <hr class="my-4">
+            <p>
+                List of answer!
+            </p>
+
+            <b-button class="mr-2" variant="primary" href="#">Submit</b-button>
+            <b-button variant="success" href="#">Next</b-button>
+        </b-jumbotron>
     </div>
 </template>
 
 <script>
-export default {
-    
-}
+    export default {
+
+    }
 </script>
