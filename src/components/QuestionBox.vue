2b202d84f8f0d6052f7670f1ee80377885b9bb35
Pooya Golchian <pooya.golchian@gmail.com>
2019-09-02 10:30:09 +0430

    Add sass loader and fix Question box select question

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -6,9 +6,19 @@
             </template>
 
             <hr class="my-4">
-            <p>
-                List of answer!
-            </p>
+            
+
+            <b-list-group class="pb-3">
+            <b-list-group-item 
+            
+            v-for="(answer, index) in answers" 
+            :key="index"
+            @click="selectAnswer(index)"
+            :class="[selectedIndex === index ? 'selected' : '']"
+            >{{answer}}
+            </b-list-group-item>
+            </b-list-group>
+
 
             <b-button class="mr-2" variant="primary" href="#">Submit</b-button>
             <b-button @click="next" variant="success" href="#">Next</b-button>
@@ -18,10 +28,38 @@
 <script>
     export default {
 
+
+
         props: {
             currentQuestion: Object,
             next: Function,
         },
+
+
+        data() {
+            return {
+                selectedIndex: null
+            }
+        },
+
+      
+        computed: {
+            answers() {
+                let answers = [...this.currentQuestion.incorrect_answers]
+                //console.log(this.currentQuestion);        
+                return answers
+            }
+        },
+
+
+        methods: {
+            selectAnswer(index) {
+                this.selectedIndex = index
+                // console.log(index)
+            }
+        },
+
+
         mounted() {
             console.log(this.currentQuestion);
             
@@ -29,6 +67,27 @@
     }
 </script>
 
+
+
+<style lang="scss">
+    
+    .list-group-item {
+        background-color: #eee;
+        cursor: pointer;
+    }
+    .selected {
+        background-color: lightblue;
+    }
+
+    .correct {
+        background-color: green;
+    }
+    .incorrect {
+        background-color: red;
+    }
+
+
+</style>
 
 
 
